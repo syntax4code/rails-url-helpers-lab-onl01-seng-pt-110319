c45635d6b7381c7766055cabf7857f434d040ff3
Warren Harper II <warrenharper2@att.net>
2020-03-28 20:28:01 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,16 +1,33 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
 
   def show
+    @student = Student.find(params[:id])
   end
+
+  def activate
+    @student = Student.find(params[:id])
+
+    if @student.active
+       @student.active = false
+     else
+       @student.active = true
+    end
+    @student.active
+    @student.save
+
+    redirect_to student_path(@student)
+  end
+
 
   private
 
     def set_student
       @student = Student.find(params[:id])
     end
-end
+
+end ### End of StudentsController Class
